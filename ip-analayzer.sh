65bd4a01c5e322519ce1808981c6125a9e353757
javiandola <96593664+javiandola@users.noreply.github.com>
2022-07-13 20:05:21 +0200

    Add files via upload

---
+++ ip-analayzer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [[ $1 == -h ]]; then
+    echo -e "Use: ip-analayzer <ip>"
+    echo -e "Example"
+    echo -e "ip-analayzer 8.8.8.8.8"
+fi    
+
+curl -s "https://www.geolocation.com/es?ip={$1}#ipresult" | html2text | grep -A 12 "countryCode"
+exit
